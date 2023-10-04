ff30accf3a1994fc7378276dced1cb456e87e933
Imran <imran.mia@portonics.com>
2023-10-04 11:50:36 +0600

    init

---
+++ Sources/KitBase/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//
+//
+//  Created by Imran on 4/10/23.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct ModalView: View {
+    public init() {}
+    public var body: some View {
+        Text("Hello ")
+    }
+}
+@available(iOS 13.0, *)
+#Preview {
+    ModalView()
+}
