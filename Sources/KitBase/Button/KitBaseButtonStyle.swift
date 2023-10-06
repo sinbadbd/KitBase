e7c733532ca0abb14d819108a93920b5cb04f7db
Imran <imran.mia@portonics.com>
2023-10-06 19:00:39 +0600

    button add

--- Sources/KitBase/Button/KitBaseButtonStyle.swift
+++ Sources/KitBase/Button/KitBaseButtonStyle.swift
@@ -1,11 +1,9 @@
 //
-//  SwiftUIView.swift
+//  KitBaseButtonStyle.swift
 //
 //
 //  Created by Imran on 6/10/23.
 //
-
-import SwiftUI
 
 import SwiftUI
 
@@ -19,22 +17,45 @@ public enum ButtonVariant {
 
 @available(iOS 15.0, *)
 public struct KitBaseButtonStyle: ButtonStyle {
+    
     public let size: ButtonSize
     public let variant: ButtonVariant
-    public let fontSize: CGFloat
-    public let backgroundColor: Color
+    public let backgroundColor: Color?
+    public let borderColor: Color?
+    public let foregroundColor: Color?
+    public let opacity: Double?
+    public let buttonWidth: CGFloat?
+    public let buttonHeight: CGFloat?
+    public let buttonCornerRadius: CGFloat?
+    public let borderWidth: CGFloat?
     
-    public init(size: ButtonSize, variant: ButtonVariant, fontSize: CGFloat, backgroundColor: Color) {
+    public init(
+        size: ButtonSize,
+        variant: ButtonVariant,
+        backgroundColor: Color? = nil,
+        borderColor: Color? = nil,
+        foregroundColor: Color? = nil,
+        opacity: Double? = nil,
+        buttonWidth: CGFloat? = nil,
+        buttonHeight: CGFloat? = nil,
+        buttonCornerRadius: CGFloat? = nil,
+        borderWidth: CGFloat? = nil
+    ) {
         self.size = size
         self.variant = variant
-        self.fontSize = fontSize
         self.backgroundColor = backgroundColor
+        self.borderColor = borderColor
+        self.foregroundColor = foregroundColor
+        self.opacity = opacity
+        self.buttonWidth = buttonWidth
+        self.buttonHeight = buttonHeight
+        self.buttonCornerRadius = buttonCornerRadius
+        self.borderWidth = borderWidth
     }
     
     public func makeBody(configuration: Configuration) -> some View {
         let padding: EdgeInsets
-        let foregroundColor: Color
-        let borderWidth: CGFloat
+        let background: Color
         
         switch size {
         case .xs:
@@ -49,33 +70,51 @@ public struct KitBaseButtonStyle: ButtonStyle {
         
         switch variant {
         case .solid:
-            foregroundColor = .white
-            borderWidth = 0
+            background = backgroundColor ?? .clear
         case .subtle:
-            foregroundColor = backgroundColor // Use background color for subtle
-            borderWidth = 1
+            background = backgroundColor ?? .clear
         case .outline:
-            foregroundColor = backgroundColor // Use background color for outline
-            borderWidth = 1
+            background = Color.clear
         }
         
         return Button(action: {}) {
             configuration.label
-                .font(.system(size: fontSize, weight: .semibold))
+                .frame(width: buttonWidth, height: buttonHeight)
                 .foregroundColor(foregroundColor)
                 .padding(padding)
-                .background(backgroundColor)
-                .cornerRadius(8)
+                .background(background)
+                .opacity(opacity ?? 1.0)
+                .cornerRadius(buttonCornerRadius ?? 8)
                 .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(foregroundColor, lineWidth: borderWidth)
+                    RoundedRectangle(cornerRadius: buttonCornerRadius ?? 8)
+                        .stroke(borderColor ?? .clear, lineWidth: borderWidth ?? 0)
                 )
         }
     }
 }
 
 
-//@available(iOS 15.0, *)
-//#Preview {
-//    KitBaseButtonStyle(size: .lg, variant: .outline) as! any View
-//}
+
+@available(iOS 15.0, *)
+#Preview {
+    ContentButtonView()
+}
+
+@available(iOS 15.0, *)
+struct ContentButtonView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            Button("Solid XS", action: {})
+                .buttonStyle(KitBaseButtonStyle(size: .lg, variant: .solid, backgroundColor: .red, borderColor: .accentColor, foregroundColor:.blue, buttonCornerRadius: 8))
+            
+            Button("Subtle MD", action: {})
+                .buttonStyle(KitBaseButtonStyle(size: .md, variant: .solid, backgroundColor: .green, borderColor: .yellow, foregroundColor: .blue, buttonWidth: UIScreen.main.bounds.width * 0.8, borderWidth: 2))
+            
+            Button("Outline SM", action: {})
+                .buttonStyle(KitBaseButtonStyle(size: .sm, variant: .outline, backgroundColor: .blue, borderColor: .red, foregroundColor: .yellow, borderWidth: 1))
+            
+            Button("SwiftUI Solid LG", action: {})
+                .buttonStyle(KitBaseButtonStyle(size: .sm, variant: .solid, foregroundColor: .red))
+        }
+    }
+}
