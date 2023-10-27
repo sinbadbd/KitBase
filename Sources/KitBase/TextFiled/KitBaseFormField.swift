124731490b3b68f0588a56942e78cd781901baa0
Imran <imran.mia@portonics.com>
2023-10-27 17:34:47 +0600

    border color for error message

--- Sources/KitBase/TextFiled/KitBaseFormField.swift
+++ Sources/KitBase/TextFiled/KitBaseFormField.swift
@@ -6,8 +6,8 @@
 //
 
 import SwiftUI
-@available(iOS 15.0, *)
 
+@available(iOS 15.0, *)
 public struct KitBaseFormField<Content>: View where Content: View {
     let title: String
     let textColor: Color
@@ -15,6 +15,7 @@ public struct KitBaseFormField<Content>: View where Content: View {
     let error: String?
     let content: () -> Content
     @Binding var isValid: Bool
+    @Binding var borderColor: Color
     
     public init(
         title: String,
@@ -22,6 +23,7 @@ public struct KitBaseFormField<Content>: View where Content: View {
         backgroundColor: Color = .white,
         error: String? = nil,
         isValid: Binding<Bool>,
+        borderColor: Binding<Color>,
         @ViewBuilder content: @escaping () -> Content
     ) {
         self.title = title
@@ -30,14 +32,15 @@ public struct KitBaseFormField<Content>: View where Content: View {
         self.error = error
         self.content = content
         self._isValid = isValid
+        self._borderColor = borderColor
     }
-    
+
     public var body: some View {
         VStack(alignment: .leading) {
             Text(title)
                 .foregroundColor(textColor)
                 .font(.caption)
-            
+
             content()
                 .frame(height: 44)
                 .foregroundColor(textColor)
@@ -48,9 +51,9 @@ public struct KitBaseFormField<Content>: View where Content: View {
                 )
                 .overlay {
                     RoundedRectangle(cornerRadius: 8, style: .continuous)
-                        .stroke(textColor, lineWidth: 1)
+                        .stroke(borderColor, lineWidth: 1)
                 }
-            
+
             if let error = error {
                 Text(error)
                     .foregroundColor(.red) // Customize the error text color
@@ -69,21 +72,29 @@ public struct KitBaseFormField<Content>: View where Content: View {
 struct TextFieldView: View {
     
     @State private var username = ""
-    @State private var password = ""
-    @State private var isUsernameValid = true
-    @State private var isPasswordValid = true
-    @State private var usernameError: String? = nil
-    @State private var passwordError: String? = nil
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            KitBaseFormField(title: "Username", error: usernameError, isValid: $isUsernameValid) {
-                TextField("Username", text: $username)
-            }
-            
-            KitBaseFormField(title: "Password", error: passwordError, isValid: $isPasswordValid) {
-                SecureField("Password", text: $password)
-            }
-        }
-    }
+       @State private var password = ""
+       @State private var isUsernameValid = true
+       @State private var isPasswordValid = true
+       @State private var usernameError: String? = nil
+       @State private var passwordError: String? = nil
+       @State private var usernameBorderColor = Color.gray
+       @State private var passwordBorderColor = Color.gray
+
+       var body: some View {
+           VStack(alignment: .leading, spacing: 8) {
+               KitBaseFormField(title: "Username", error: usernameError, isValid: $isUsernameValid, borderColor: $usernameBorderColor) {
+                   TextField("Username", text: $username)
+               }
+               .onChange(of: isUsernameValid) { newValue in
+                   usernameBorderColor = newValue ? Color.gray : .red
+               }
+
+               KitBaseFormField(title: "Password", error: passwordError, isValid: $isPasswordValid, borderColor: $passwordBorderColor) {
+                   SecureField("Password", text: $password)
+               }
+               .onChange(of: isPasswordValid) { newValue in
+                   passwordBorderColor = newValue ? Color.gray : .red
+               }
+           }
+       }
 }
