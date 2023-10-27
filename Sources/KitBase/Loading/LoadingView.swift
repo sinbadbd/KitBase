a0f7544319622844502ffe8d72f38293fa2fb42a
Imran <imran.mia@portonics.com>
2023-10-27 12:29:48 +0600

    loading effect bug fix

--- Sources/KitBase/Loading/LoadingView.swift
+++ Sources/KitBase/Loading/LoadingView.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 @available(iOS 15.0, *)
 public struct LoadingView: View {
-    
-    public var text: String
-    public init(text: String) {
-        self.text = text
+    public var progressColor: Color
+    init(progressColor: Color) {
+        self.progressColor = progressColor
     }
+    
     public var body: some View {
         ZStack {
             Color.black.opacity(0.5)
                 .ignoresSafeArea()
             VStack {
-                ProgressView(text)
+                ProgressView()
+                    .tint(progressColor)
+                    .progressViewStyle(.circular)
+                    .scaleEffect(x:2.5, y: 2.5)
+                    .frame(width: 50, height: 50)
                     .foregroundColor(.white)
                     .padding()
-                    .background(Color.black.opacity(0.7))
+                    .background(Color.white.opacity(0.2))
                     .cornerRadius(10)
             }
         }
@@ -30,5 +34,23 @@ public struct LoadingView: View {
 
 @available(iOS 15.0, *)
 #Preview {
-    LoadingView(text: "Loading...")
+    DemoLoadingView()
 }
+
+@available(iOS 15.0, *)
+struct DemoLoadingView: View {
+    @State private var isLoading = true
+    var body: some View {
+        ZStack{
+            Color.black.opacity(0.4)
+                .ignoresSafeArea()
+            
+            // Loading view
+            if isLoading {
+                LoadingView(progressColor: .green)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }
+    }
+}
+
