139bfb646b87dfa4ef80e7d42a6c076155a0e014
Imran <imran.mia@portonics.com>
2023-10-27 16:58:42 +0600

    change some error logic

--- Sources/KitBase/TextFiled/KitBaseFormField.swift
+++ Sources/KitBase/TextFiled/KitBaseFormField.swift
@@ -9,19 +9,29 @@ import SwiftUI
 @available(iOS 15.0, *)
 
 public struct KitBaseFormField<Content>: View where Content: View {
-    
     let title: String
     let textColor: Color
     let backgroundColor: Color
+    let error: String?
     let content: () -> Content
+    @Binding var isValid: Bool
     
-    public init(title: String, textColor: Color = .gray, backgroundColor: Color = .white, @ViewBuilder content: @escaping () -> Content) {
+    public init(
+        title: String,
+        textColor: Color = .gray,
+        backgroundColor: Color = .white,
+        error: String? = nil,
+        isValid: Binding<Bool>,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
         self.title = title
         self.textColor = textColor
         self.backgroundColor = backgroundColor
+        self.error = error
         self.content = content
+        self._isValid = isValid
     }
- 
+    
     public var body: some View {
         VStack(alignment: .leading) {
             Text(title)
@@ -40,6 +50,12 @@ public struct KitBaseFormField<Content>: View where Content: View {
                     RoundedRectangle(cornerRadius: 8, style: .continuous)
                         .stroke(textColor, lineWidth: 1)
                 }
+            
+            if let error = error {
+                Text(error)
+                    .foregroundColor(.red) // Customize the error text color
+                    .font(.caption)
+            }
         }
     }
 }
@@ -54,26 +70,19 @@ struct TextFieldView: View {
     
     @State private var username = ""
     @State private var password = ""
-    @State private var firstname = ""
-    @State private var lastname = ""
+    @State private var isUsernameValid = true
+    @State private var isPasswordValid = true
+    @State private var usernameError: String? = nil
+    @State private var passwordError: String? = nil
     
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
-            KitBaseFormField(title: "Username") {
-                TextField("username", text: $username)
+            KitBaseFormField(title: "Username", error: usernameError, isValid: $isUsernameValid) {
+                TextField("Username", text: $username)
             }
             
-            KitBaseFormField(title: "Password") {
+            KitBaseFormField(title: "Password", error: passwordError, isValid: $isPasswordValid) {
                 SecureField("Password", text: $password)
-                    .keyboardType(.namePhonePad)
-            }
-            
-            KitBaseFormField(title: "First name") {
-                TextField("First name", text: $firstname)
-            }
-            
-            KitBaseFormField(title: "Last name") {
-                TextField("Last name", text: $lastname)
             }
         }
     }
