61f8a730ff68f4726d83b31c64fa8ac9a80bbb93
Imran <imufun2@gmail.com>
2023-12-29 20:37:29 +0600

    add example project

---
+++ KitBaseExamle/KitBaseExamle/KitBaseExamleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitBaseExamleApp.swift
+//  KitBaseExamle
+//
+//  Created by Imran on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct KitBaseExamleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
