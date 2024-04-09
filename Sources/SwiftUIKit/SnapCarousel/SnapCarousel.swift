ee7f3ba4644daf9759f8f713a33336fc5c8706c0
Imran <imufun2@gmail.com>
2024-04-09 18:22:31 +0600

    Refactor tabs

--- Sources/SwiftUIKit/SnapCarousel/SnapCarousel.swift
+++ Sources/SwiftUIKit/SnapCarousel/SnapCarousel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 public struct SnapCarousel<Content: View, T: Identifiable>: View {
     var content: (T) -> Content
--- Sources/SwiftUIKit/SnapCarousel/SnapCarouselBuilder.swift
+++ Sources/SwiftUIKit/SnapCarousel/SnapCarouselBuilder.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 public class SnapCarouselBuilder<Content: View, T: Identifiable> {
     
